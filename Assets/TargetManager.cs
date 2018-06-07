7ba8936d83cef08b93566404497fd3558241c369
Valentin <valentin.kao@epitech.eu>
2018-06-07 11:37:59 +0800

    Improvement on Scene Management - Starting Task Scene

---
+++ Assets/TargetManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : Singleton<TargetManager>
+{
+    [SerializeField]
+    private GameObject target;
+
+
+}
